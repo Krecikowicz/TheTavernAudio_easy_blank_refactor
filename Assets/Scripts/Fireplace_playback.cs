0067d270cf3715cab12ce322769e9c9e3f02cc17
Jakub Misiak <kuba1misiak@gmail.com>
2025-11-25 19:44:09 +0100

    faraon x shadow bifek part 54

--- Assets/Scripts/Fireplace_playback.cs
+++ Assets/Scripts/Fireplace_playback.cs
@@ -8,11 +8,11 @@ public class Fireplace_playback : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
-        fireplaceEmitter.SetParameter("Fire", 0);
+        fireplaceEmitter.SetParameter("ogien", 1);
     }
 
     private void OnTriggerExit(Collider other)
     {
-        fireplaceEmitter.SetParameter("Fire", 1);
+        fireplaceEmitter.SetParameter("ogien", 0);
     }
 }
